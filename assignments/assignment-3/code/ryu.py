32cf6cc4851ffc85295b154a6177af617b8d22f7
Abdullah Al Raqibul Islam <iraqibul.csedu@gmail.com>
2021-03-22 19:59:57 -0400

    Assignment-3: Add basic code for topo learning in the ryu

--- assignments/assignment-3/code/ryu.py
+++ assignments/assignment-3/code/ryu.py
@@ -15,23 +15,40 @@
 
 from ryu.base import app_manager
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
 
+from ryu.topology import event
+# Below is the library used for topo discovery
+from ryu.topology.api import get_switch, get_link
+import copy
 
 class SimpleSwitch13(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch13, self).__init__(*args, **kwargs)
+        # USed for learning switch functioning
         self.mac_to_port = {}
+        # Holds the topology data and structure
+        self.topo_raw_switches = []
+        self.topo_raw_links = []
+
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
+        msg = ev.msg
+        self.logger.info('OFPSwitchFeatures received: '
+                         '\n\tdatapath_id=0x%016x n_buffers=%d '
+                         '\n\tn_tables=%d auxiliary_id=%d '
+                         '\n\tcapabilities=0x%08x',
+                         msg.datapath_id, msg.n_buffers, msg.n_tables,
+                         msg.auxiliary_id, msg.capabilities)
+
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -48,6 +65,19 @@ class SimpleSwitch13(app_manager.RyuApp):
                                           ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
 
+    # We are not using this function
+    def delete_flow(self, datapath):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        for dst in self.mac_to_port[datapath.id].keys():
+            match = parser.OFPMatch(eth_dst=dst)
+            mod = parser.OFPFlowMod(
+                datapath, command=ofproto.OFPFC_DELETE,
+                out_port=ofproto.OFPP_ANY, out_group=ofproto.OFPG_ANY,
+                priority=1, match=match)
+            datapath.send_msg(mod)
+
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -63,10 +93,12 @@ class SimpleSwitch13(app_manager.RyuApp):
                                     match=match, instructions=inst)
         datapath.send_msg(mod)
 
+    """
+    This is called when Ryu receives an OpenFlow packet_in message. The trick is set_ev_cls decorator. This decorator
+    tells Ryu when the decorated function should be called.
+    """
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
-        # If you hit this you might want to increase
-        # the "miss_send_length" of your switch
         if ev.msg.msg_len < ev.msg.total_len:
             self.logger.debug("packet truncated: only %s of %s bytes",
                               ev.msg.msg_len, ev.msg.total_len)
@@ -85,10 +117,10 @@ class SimpleSwitch13(app_manager.RyuApp):
         dst = eth.dst
         src = eth.src
 
-        dpid = format(datapath.id, "d").zfill(16)
+        dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
 
-        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+        # self.logger.info("\tpacket in %s %s %s %s", dpid, src, dst, in_port)
 
         # learn a mac address to avoid FLOOD next time.
         self.mac_to_port[dpid][src] = in_port
@@ -102,7 +134,7 @@ class SimpleSwitch13(app_manager.RyuApp):
 
         # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(in_port=in_port, eth_dst=dst, eth_src=src)
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
             # verify if we have a valid buffer_id, if yes avoid to send both
             # flow_mod & packet_out
             if msg.buffer_id != ofproto.OFP_NO_BUFFER:
@@ -117,3 +149,42 @@ class SimpleSwitch13(app_manager.RyuApp):
         out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
                                   in_port=in_port, actions=actions, data=data)
         datapath.send_msg(out)
+
+    ###################################################################################
+    """
+    The event EventSwitchEnter will trigger the activation of get_topology_data().
+    """
+    @set_ev_cls(event.EventSwitchEnter)
+    def handler_switch_enter(self, ev):
+        # The Function get_switch(self, None) outputs the list of switches.
+        self.topo_raw_switches = copy.copy(get_switch(self, None))
+        # The Function get_link(self, None) outputs the list of links.
+        self.topo_raw_links = copy.copy(get_link(self, None))
+
+        """
+        Now you have saved the links and switches of the topo. So you could do all sort of stuf with them. 
+        """
+
+        print(" \t" + "Current Links:")
+        for l in self.topo_raw_links:
+            print (" \t\t" + str(l))
+
+        print(" \t" + "Current Switches:")
+        for s in self.topo_raw_switches:
+            print (" \t\t" + str(s))
+
+    """
+    This event is fired when a switch leaves the topo. i.e. fails.
+    """
+    @set_ev_cls(event.EventSwitchLeave, [MAIN_DISPATCHER, CONFIG_DISPATCHER, DEAD_DISPATCHER])
+    def handler_switch_leave(self, ev):
+        self.logger.info("Not tracking Switches, switch leaved.")
+
+    # @set_ev_cls(event.EventSwitchEnter)
+    # def get_topology_data(self, ev):
+    #     switch_list = get_switch(self.topology_api_app, None)
+    #     switches = [switch.dp.id for switch in switch_list]
+    #     links_list = get_link(self.topology_api_app, None)
+    #     links = [(link.src.dpid, link.dst.dpid, {'port': link.src.port_no}) for link in links_list]
+    #     print("links: ".format(links))
+    #     print("switches: ".format(switches))
